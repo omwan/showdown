9cb3c683da566f1e8b740a7a717cc690bd974a6a
koucaco <hwang.ch@husky.neu.edu>
2019-02-18 12:32:34 -0500

    front-end css and component logic

--- assets/js/showdown-game.jsx
+++ assets/js/showdown-game.jsx
@@ -5,7 +5,8 @@ import _ from 'lodash';
 export default function game_init(root, channel) {
     ReactDOM.render(<Showdown channel={channel}/>, root);
 }
-
+let debug = false;
+let teams = false; // teams yet to be implemented
 class Showdown extends React.Component {
     constructor(props) {
         super(props);
@@ -14,7 +15,6 @@ class Showdown extends React.Component {
             users: [],
             submitted_moves: []
         };
-
         this.channel
             .join()
             .receive("ok", this.got_view.bind(this))
@@ -26,13 +26,14 @@ class Showdown extends React.Component {
     got_view(view) {
         console.log(view);
         this.setState(view.game);
+        console.log(this.state);
     }
 
     render() {
         return <div>
-            { false && <WaitingRoom></WaitingRoom> }
-            { true && <Battle></Battle> }
-            { false && <Result></Result>}
+            { debug && <RestartButton></RestartButton>}
+            { !this.state.opponent && <WaitingRoom></WaitingRoom> }
+            { this.state.opponent && <Battle state={this.state}></Battle> }
 
         </div>
     }
@@ -53,16 +54,19 @@ class Battle extends React.Component {
     constructor(props) {
         super(props);
         this.channel = props.channel;
-        this.state = {};
+        this.player = props.state.player;
+        this.opponent = props.state.opponent;                
     }
     render() {
         return <div className="battle">
-                <div className="tab">It's time to D-d-d-d-d-d-duel!</div>
-                <img className="artwork" src="https://cdn.discordapp.com/attachments/405465305822003201/546814731944591371/image0.jpg"></img>
-                <PkmInfoBar></PkmInfoBar>
-                <PkmInfoBar></PkmInfoBar>
-                <img className="artwork" src="https://cdn.discordapp.com/attachments/405465305822003201/546814731944591371/image0.jpg"></img>
-                <Moveset></Moveset>
+    { teams && <Team name={this.player.name} team={this.player.team} classname="player"></Team> }
+    { teams && <Team name={this.opponent.name} team={this.opponent.team} classname="opponent"></Team> }
+                <img className="artwork player" src="https://cdn.discordapp.com/attachments/405465305822003201/546814731944591371/image0.jpg"></img>
+                <PkmInfoBar pokemon={this.player.current_pokemon} classname="player"></PkmInfoBar>
+                <PkmInfoBar pokemon={this.opponent.current_pokemon}  classname="opponent"></PkmInfoBar>
+                <img className="artwork opponent" src="https://cdn.discordapp.com/attachments/405465305822003201/546814731944591371/image0.jpg"></img>
+                { true && <BattleText></BattleText>}
+                <Menu team={this.player.team} moves={this.player.current_pokemon.moves} ></Menu>
             </div>
     }
 }
@@ -71,12 +75,14 @@ class PkmInfoBar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
+        this.pokemon = props.pokemon;
+        this.class = this.props.classname + " info-bar";
         
     }
     render() {
-        return <div className="info-bar">
-            <div className="pkm-name">YOLO</div>
-            <div className="pkm-hp">hp = 69/420 ayyyy blaze it lmao</div>
+        return <div className={this.class}>
+            <div className="pkm-name">{this.pokemon.name}</div>
+            <div className="pkm-hp">current_hp / {this.pokemon.hp}</div>
         </div>
     }
 }
@@ -85,11 +91,12 @@ class Moveset extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
+        this.moves = props.moves;
     }
     render() {
-        return <div className="Moveset">
-            <Move></Move>
-            <Move></Move>
+        return <div className="moveset">
+            <Move move={this.moves[0]} classname="move-1"></Move>
+            <Move move={this.moves[1]}  classname="move-2"></Move>
         </div>
     }
 }
@@ -99,34 +106,114 @@ class Move extends React.Component {
         super(props);
         this.channel = props.channel;
         this.state = {};
+        this.class = this.props.classname + " move";
+        this.move = props.move;
+
     }
+
+    handleClick(move) {
+        console.log(move);
+        // TODO: send move to server
+    }
+
     render() {
-        return <div>
-            <div className="move-name">Blaze it</div>
-            <div className="move-type">FIRE</div>
+        return <div className={this.class} onClick={(e) => this.handleClick(this.move.name)}>
+            <div className="move-name">{this.move.name}</div>
+            <div className="move-info">
+                <div className="move-type">{this.move.type}</div>
+                <div className="move-power">power: {this.move.power}</div>
+
+            </div>
+            
         </div>
     }
     
 }
-class Result extends React.Component {
-    constructor(props) {
-        super(props);
-        this.channel = props.channel;
-        this.state = {};
-    }
-    // result + button to redirect to / ?
-    render() {
-        return <div className="redirect">
+// class Result extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.channel = props.channel;
+//         this.state = {};
+//     }
+//     // result + button to redirect to / ?
+//     render() {
+//         return <div className="redirect">
             
-        </div>
-    }
-}
+//         </div>
+//     }
+// }
 
 class Team extends React.Component {
     constructor(props) {
         super(props);
         this.channel = props.channel;
         this.state = {};
+        this.class = this.props.classname + " team";
     }
 
+    render() {
+        return <div className={this.class}></div>
+    }
+}
+
+class BattleText extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.channel = props.channel;
+        this.state = {};
+        this.class = "battle-text";
+    }
+
+    render() {
+        return <div className={this.class}></div>
+    }
+}
+
+class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.channel = props.channel;
+        this.state = {
+            menu: ''
+        };
+        this.team = props.team;
+        this.moves = props.moves;
+    }
+
+    handleClick(text) {
+        this.setState({menu: text});
+        console.log(this.state);
+    }
+
+    render() {
+        return <div className="menu">
+        {this.state.menu != '' && <button onClick={ (e) => this.handleClick('')}>back to menu</button>}
+        <div class="submenu">
+            {this.state.menu == '' && <button onClick={ (e) => this.handleClick('moves')}>moves</button>}
+            {teams && this.state.menu == '' && <button onClick={ (e) => this.handleClick('pokemon')}>pokemon</button>}
+            {this.state.menu == 'moves' && <Moveset moves={this.moves}></Moveset>}
+            {this.state.menu == 'pokemon'  && <SwitchPkm team={this.team}></SwitchPkm>}</div>
+        </div>
+    }
+}
+
+class RestartButton extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    // insert restart button here for debug purposes
+    render() {
+        return <button></button>
+    }
+}
+
+class SwitchPkm extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div></div>
+    }
 }
